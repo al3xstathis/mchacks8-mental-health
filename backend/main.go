ad4a89826bc60a5c81fb170b9d780a0186c75fd8
Nicolas MacBeth <nicolasmacbeth@gmail.com>
2021-01-30 17:33:07 -0500

    logic for sending/receiving websocket messages

--- backend/main.go
+++ backend/main.go
@@ -9,25 +9,38 @@ import (
 )
 
 type message struct {
-	UID string `json:"uid"`
+	User    userStruct `json:"user"`
+	Payload string     `json:"payload"`
+	Type    string     `json:"type"`
+	Target  string     `json:"target"`
+}
+
+type userStruct struct {
+	UID      string   `json:"uid"`
+	Username string   `json:"username"`
+	Keywords []string `json:"keywords"`
+	WS       *websocket.Conn
 }
 
 const port = 3030
 
-var clients = make(map[*websocket.Conn]bool)
+var clients = make(map[*websocket.Conn]userStruct)
+var userQueue []userStruct
 var msgQueue = make(chan message)
-var upgrader = websocket.Upgrader{}
+var upgrader = websocket.Upgrader{
+	CheckOrigin: func(r *http.Request) bool { return true },
+}
 
 func main() {
 	http.HandleFunc("/ws", handleConnections)
 
 	go handleIncomingMessages()
 
-	fmt.Printf("Starting server on port %d", port)
+	fmt.Printf("Starting server on port %d\n", port)
 
 	err := http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
 	if err != nil {
-		log.Fatal("ListenAndServe error:", err)
+		log.Fatalln("ListenAndServe error:", err)
 	}
 }
 
@@ -35,11 +48,13 @@ func handleConnections(w http.ResponseWriter, r *http.Request) {
 	// Upgrade initial GET request to a websocket
 	ws, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
-		log.Fatal("WebSocket upgrader error", err)
+		if !websocket.IsUnexpectedCloseError(err, websocket.CloseGoingAway, websocket.CloseAbnormalClosure) {
+			log.Fatal("WebSocket upgrader error\n", err)
+		}
 	}
 	defer ws.Close()
 
-	clients[ws] = true
+	clients[ws] = userStruct{}
 
 	decodeAndPassMessage(ws)
 }
@@ -49,10 +64,14 @@ func decodeAndPassMessage(ws *websocket.Conn) {
 		var msg message
 		err := ws.ReadJSON(&msg)
 		if err != nil {
-			log.Printf("error: %v", err)
-			delete(clients, ws)
+			log.Printf("JSON read error: %v\n", err)
 			break
 		}
+
+		if msg.Type == "enqueue" {
+			msg.User.WS = ws
+		}
+
 		msgQueue <- msg
 	}
 }
@@ -61,14 +80,65 @@ func handleIncomingMessages() {
 	for {
 		msg := <-msgQueue
 
-		// TODO: logic here will change
-		for client := range clients {
-			err := client.WriteJSON(msg)
-			if err != nil {
-				fmt.Printf("error: %v", err)
-				client.Close()
-				delete(clients, client)
+		fmt.Printf("Message: %v\n", msg)
+
+		switch msg.Type {
+		case "enqueue":
+			clients[msg.User.WS] = msg.User
+
+			matchFound, matchedUser, keywords := searchForMatch(msg.User)
+
+			if matchFound {
+				fmt.Println("Match found!", keywords)
+
+				notifyMatches(msg.User, *matchedUser, keywords)
+			} else {
+				userQueue = append(userQueue, msg.User)
 			}
+
 		}
 	}
+}
+
+func notifyMatches(user1 userStruct, user2 userStruct, keywords []string) {
+	for ws := range clients {
+		if clients[ws].UID == user2.UID {
+			ws.WriteJSON(message{
+				Type: "matchFound",
+				User: user1,
+			})
+		}
+	}
+}
+
+func searchForMatch(user userStruct) (bool, *userStruct, []string) {
+	for i, u := range userQueue {
+		overlap, keywords := checkOverlap(user.Keywords, u.Keywords)
+		if overlap {
+			userQueue = append(userQueue[:i], userQueue[i+1:]...)
+			return true, &u, keywords
+		}
+	}
+
+	return false, nil, nil
+}
+
+func checkOverlap(keywords1 []string, keywords2 []string) (bool, []string) {
+	exists := make(map[string]bool)
+	results := []string{}
+
+	for _, v := range keywords1 {
+		exists[v] = true
+	}
+
+	for _, v := range keywords2 {
+		if exists[v] {
+			results = append(results, v)
+		}
+	}
+
+	if len(results) > 0 {
+		return true, results
+	}
+	return false, nil
 }
--- frontend/src/pages/home.js
+++ frontend/src/pages/home.js
@@ -1,7 +1,16 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import Navbar from "../components/nav";
+import {connect} from '../scripts/websocket';
 
 const Home = () => {
+    useEffect(() => {
+        connect({
+            uid: '123456',
+            username: 'Nicolas',
+            keywords: ['riperoni', 'jabronis', 'lorem', 'ipsum']
+        }, true);
+    });
+
     return (
         <>
             <Navbar/>
