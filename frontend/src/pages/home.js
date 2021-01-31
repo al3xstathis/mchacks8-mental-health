7313df2203502e932b4a34241b470ce89a2b5e3d
Dina Khalesi <dinakhalesit@gmail.com>
2021-01-30 20:00:50 -0500

    fixed sign up

--- frontend/src/pages/home.js
+++ frontend/src/pages/home.js
@@ -1,12 +1,15 @@
-import React, {useEffect} from 'react';
+import React, { useEffect, useContext } from 'react';
 import Navbar from "../components/nav";
-import {connect} from '../scripts/websocket';
+import { connect } from '../scripts/websocket';
 import Loading from "../components/loading";
 import './home.css'
-import {motion} from "framer-motion";
+import { motion } from "framer-motion";
+import { AuthContext } from "../Auth";
 
 const Home = () => {
+    const { currentUser } = useContext(AuthContext);
     useEffect(() => {
+        console.log(currentUser)
         connect({
             uid: '123456',
             username: 'Nicolas',
@@ -16,15 +19,15 @@ const Home = () => {
 
     return (
         <>
-            <Navbar/>
+            <Navbar />
             <div className="home-screen">
-                <Loading/>
+                <Loading />
                 <div className="home-text">
                     <motion.h2
-                        initial={{opacity: 0, y: 10}}
-                        animate={{opacity: 1, y: 0}}
-                        transition={{delay: 0.5}}
-                        className="home-h2">Based on the categories you chose when creating an account,<br/>
+                        initial={{ opacity: 0, y: 10 }}
+                        animate={{ opacity: 1, y: 0 }}
+                        transition={{ delay: 0.5 }}
+                        className="home-h2">Based on the categories you chose when creating an account,<br />
                         we are matching you with someone who chose similar categories</motion.h2>
                 </div>
             </div>
