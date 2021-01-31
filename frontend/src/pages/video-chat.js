1c0af11712638ed47a6f4ebd47495396ad69bff6
Alex Stathis <alexander.stathis@mail.mcgill.ca>
2021-01-30 22:17:27 -0500

    Shuffle through list of questions you can ask on video chat page

--- frontend/src/pages/video-chat.js
+++ frontend/src/pages/video-chat.js
@@ -7,6 +7,27 @@ import {motion} from 'framer-motion'
 const Video = () => {
     const [iceBreaker, setIceBreaker] = useState("Click this button to shuffle through a list of questions you could ask!");
 
+    const shuffleCards = () => {
+        const random = Math.floor(iceBreakers.length * Math.random());
+        setIceBreaker(iceBreakers[random]);
+        iceBreakers.splice(random);
+    }
+
+    const iceBreakers = [
+        "What do you most connect with? Why?",
+        "What are you committed to?",
+        "How different would your life be if there weren’t any criticism in the world?",
+        "What are you avoiding?",
+        "If joy became the national currency, what kind of work would make you wealthy?",
+        "Would you rather have less work to do or more work you enjoy doing?",
+        "How do you manage stress?",
+        "What do you believe stands between you and complete happiness?",
+        "Are you afraid to speak your own opinion?",
+        "If you could fix one world problem, what would it be?",
+        "What is your life story?",
+        "How do you deal with your mental health issues?"
+    ]
+
 
     return (
         <>
@@ -32,6 +53,7 @@ const Video = () => {
                         className="text-popup">
                         <p className="ice-breaker">{iceBreaker}</p>
                         <motion.button
+                            onClick={() => shuffleCards()}
                             whileTap={{scale: 1.7}}
                             className="shuffle">Shuffle</motion.button>
                     </motion.div>
