930700c509ab3c4832e5ebea59ba397891214429
Alex Stathis <alexander.stathis@mail.mcgill.ca>
2021-01-30 21:57:32 -0500

    Shadows and animations

--- frontend/src/pages/video-chat.js
+++ frontend/src/pages/video-chat.js
@@ -1,29 +1,41 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './video-chat.css';
 import Navbar from "../components/nav";
 import Chat from "../components/chat";
 import {motion} from 'framer-motion'
 
 const Video = () => {
+    const [iceBreaker, setIceBreaker] = useState("Click this button to shuffle through a list of questions you could ask!");
+
+
     return (
         <>
             <Navbar/>
             <div className="video-chat">
                 <div className="video-feed">
                     <motion.div
-                        initial={{opacity: 0,x: -100}}
-                        animate={{opacity: 1,x: 0}}
-                        transition={{duration:0.5}}
+                        initial={{opacity: 0, x: -100}}
+                        animate={{opacity: 1, x: 0}}
+                        transition={{duration: 0.5}}
                         className="incoming-vid">
-
                     </motion.div>
                     <motion.div
-                        initial={{opacity: 0,x: 100}}
-                        animate={{opacity: 1,x: 0}}
-                        transition={{duration:0.5}}
+                        initial={{opacity: 0, x: 100}}
+                        animate={{opacity: 1, x: 0}}
+                        transition={{duration: 0.5}}
                         className="selfie-vid">
-
                     </motion.div>
+                    <motion.div
+                        initial={{opacity: 0, x: 100}}
+                        animate={{opacity: 1, x: 0}}
+                        transition={{duration: 0.5}}
+                        className="text-popup">
+                        <p className="ice-breaker">{iceBreaker}</p>
+                        <motion.button
+                            whileTap={{scale: 1.7}}
+                            className="shuffle">Shuffle</motion.button>
+                    </motion.div>
+
                 </div>
                 <div className="chat-game-feed">
                     <Chat/>
